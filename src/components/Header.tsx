4ad35d73798cec0ebef9fd8b5c71fa1e0e0f2cd4
sinox2003 <mohaennouass@gmail.com>
2024-12-13 19:07:09 +0100

    removing useId from local storage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,6 +19,8 @@ const Header = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("authToken"); // Remove token
+    localStorage.removeItem("userId"); // Remove token
+    localStorage.removeItem("userSubject"); // Remove token
     setIsLoggedIn(false); // Update login status
     navigate("/");
   };
